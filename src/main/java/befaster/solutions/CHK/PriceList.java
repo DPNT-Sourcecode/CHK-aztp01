d1fb53c4d9716dbe940e4d0ded2e4569c8a36f5e
sbx_user1059 <sbx_user1059@ip-10-129-107-232.eu-west-2.compute.internal>
2018-09-09 19:18:32 +0000

    Sun Sep 09 19:18:32 UTC 2018

--- src/main/java/befaster/solutions/CHK/PriceList.java
+++ src/main/java/befaster/solutions/CHK/PriceList.java
@@ -1,18 +1,18 @@
 package befaster.solutions.CHK;
 
 public enum PriceList {
-    A(50),
-    B(30),
-    C(20),
-    D(15);
+    A(50, 3, 130),
+    B(30, 2, 45),
+    C(20, 1, 20),
+    D(15, 1, 20);
 
     private int price;
 
-    PriceList(int price) {
+    PriceList(int price, int special, int newPrice) {
         this.price = price;
     }
 
     public int getPrice() {
         return price;
     }
-}
+}
