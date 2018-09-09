66206cb4f10cc62f7555ca2063174c0d4db427b2
sbx_user1059 <sbx_user1059@ip-10-129-107-232.eu-west-2.compute.internal>
2018-09-09 18:35:09 +0000

    Sun Sep 09 18:35:09 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,5 +1,20 @@
+package befaster.solutions.HLO;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HelloSolutionTest {
+    private HelloSolution test;
 
+    @Before
+    public void setUp() throws Exception {
+        test = new HelloSolution();
+    }
+
+    @Test
+    public void hello() {
+        assertEquals(test.hello("Friend"), "Hello World!");
+    }
 }
