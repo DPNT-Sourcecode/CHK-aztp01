96d163dec631fcab6e17d84ed4d44e410088c90e
sbx_user1059 <sbx_user1059@ip-10-129-107-232.eu-west-2.compute.internal>
2018-09-09 19:07:20 +0000

    Sun Sep 09 19:07:20 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,8 +8,8 @@ public class CheckoutSolution {
         Map<String, Integer> itemCount = new HashMap<>();
 
         for (int x = 0; x < skus.length(); x++) {
-            
-            itemCount.merge(String.valueOf(skus.charAt(x)), 1, (key, value) -> value = value + 1);
+
+            itemCount.merge(String.valueOf(skus.charAt(x)), 1, (key, value) -> value + 1);
         }
 
         int sum = 0;
