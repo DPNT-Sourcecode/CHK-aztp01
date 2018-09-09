5a3d8210c9b561c837bd872e1daead0f6df7abec
sbx_user1060 <sbx_user1060@ip-10-129-16-101.eu-west-2.compute.internal>
2018-09-09 21:03:41 +0000

    Sun Sep 09 21:03:41 UTC 2018

--- src/main/java/befaster/solutions/CHK/Item.java
+++ src/main/java/befaster/solutions/CHK/Item.java
@@ -1,4 +1,57 @@
 package befaster.solutions.CHK;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Item {
-}
+    private String name;
+    private Integer price;
+    private List<Offer> offers;
+
+    public Item(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+        this.offers = new LinkedList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void addOffer(Integer size, Integer price) {
+        offers.add(new Offer(size, price));
+    }
+
+    public Integer priceFor(int num) {
+        return 0;
+    }
+
+    private class Offer implements Comparable<Offer> {
+        private Integer offerSize;
+        private Integer offerPrice;
+
+        Offer(Integer offerSize, Integer offerPrice) {
+            this.offerPrice = offerPrice;
+            this.offerSize = offerSize;
+        }
+
+        public Integer getOfferSize() {
+            return offerSize;
+        }
+
+        public Integer getOfferPrice() {
+            return offerPrice;
+        }
+
+
+        @Override
+        public int compareTo(Offer o) {
+            return this.offerSize.compareTo(o.offerSize);
+        }
+    }
+
+}
--- src/test/java/befaster/solutions/CHK/ItemTest.java
+++ src/test/java/befaster/solutions/CHK/ItemTest.java
@@ -1,5 +1,26 @@
+package befaster.solutions.CHK;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ItemTest {
+    private Item test;
 
+    @Test
+    public void priceFor() {
+        test = new Item("A", 20);
+
+        assertEquals(60, test.priceFor(3).longValue());
+
+        test.addOffer(2, 30);
+    }
+
+    @Test
+    public void priceForWithOffers() {
+        test = new Item("A", 20);
+        test.addOffer(2, 30);
+
+        assertEquals(50, test.priceFor(3).longValue());
+    }
 }
