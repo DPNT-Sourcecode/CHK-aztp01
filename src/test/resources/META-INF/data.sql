6fef662ae2401e992add3b898e648bdea95f1886
sbx_user1060 <sbx_user1060@ip-10-128-93-186.eu-west-2.compute.internal>
2018-09-16 22:56:48 +0000

    Sun Sep 16 22:56:48 UTC 2018

--- src/test/resources/META-INF/data.sql
+++ src/test/resources/META-INF/data.sql
@@ -1,6 +1,11 @@
 INSERT INTO Item (name, price) VALUES ('A', 50);
+INSERT INTO Offer (item, offerSize, offerPrice, item) VALUES (1, "A", 3, 130, "");
+INSERT INTO Offer (item, offerSize, offerPrice, item) VALUES (2, "A", 5, 200, "");
+
 INSERT INTO Item (name, price) VALUES ('B', 30);
+
 INSERT INTO Item (name, price) VALUES ('C', 20);
 INSERT INTO Item (name, price) VALUES ('D', 15);
 INSERT INTO Item (name, price) VALUES ('E', 40);
 INSERT INTO Item (name, price) VALUES ('F', 10);
+
