1004a69a17bebb6c7ca54940af12d6f8d944e0b7
sbx_user1060 <sbx_user1060@ip-10-128-93-186.eu-west-2.compute.internal>
2018-09-16 22:50:48 +0000

    Sun Sep 16 22:50:48 UTC 2018

--- src/test/resources/META-INF/data.sql
+++ src/test/resources/META-INF/data.sql
@@ -1,2 +1,6 @@
-INSERT INTO Item (name, price) VALUES ('A', 32);
-INSERT INTO Item (name, price) VALUES ('B', 3);
+INSERT INTO Item (name, price) VALUES ('A', 50);
+INSERT INTO Item (name, price) VALUES ('B', 30);
+INSERT INTO Item (name, price) VALUES ('C', 20);
+INSERT INTO Item (name, price) VALUES ('D', 15);
+INSERT INTO Item (name, price) VALUES ('E', 40);
+INSERT INTO Item (name, price) VALUES ('F', 10);
