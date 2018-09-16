61de3ef26311ab0d7985a13cad6b3d5591077b59
sbx_user1060 <sbx_user1060@ip-10-128-93-186.eu-west-2.compute.internal>
2018-09-16 23:02:48 +0000

    Sun Sep 16 23:02:48 UTC 2018

--- src/test/resources/META-INF/data.sql
+++ src/test/resources/META-INF/data.sql
@@ -1,10 +1,20 @@
 INSERT INTO Item (name, price) VALUES ('A', 50);
 INSERT INTO Offer (id, item, offer_size, offer_price, offer_item) VALUES (1, 'A', 3, 130, '');
 INSERT INTO Offer (id, item, offer_size, offer_price, offer_item) VALUES (2, 'A', 5, 200, '');
+
 INSERT INTO Item (name, price) VALUES ('B', 30);
+INSERT INTO Offer (id, item, offer_size, offer_price, offer_item) VALUES (3, 'B', 2, 45, '');
 
 INSERT INTO Item (name, price) VALUES ('C', 20);
+INSERT INTO Offer (id, item, offer_size, offer_price, offer_item) VALUES (1, 'A', 3, 130, '');
+
 INSERT INTO Item (name, price) VALUES ('D', 15);
+INSERT INTO Offer (id, item, offer_size, offer_price, offer_item) VALUES (1, 'A', 3, 130, '');
+
 INSERT INTO Item (name, price) VALUES ('E', 40);
+INSERT INTO Offer (id, item, offer_size, offer_price, offer_item) VALUES (1, 'A', 3, 130, '');
+
 INSERT INTO Item (name, price) VALUES ('F', 10);
+INSERT INTO Offer (id, item, offer_size, offer_price, offer_item) VALUES (1, 'A', 3, 130, '');
+
 
